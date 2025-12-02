1cee9fae3b24a30124e082a47742eef694b3736a
kienquoc-uit <22521209@gm.uit.edu.vn>
2025-12-02 16:11:38 +0700

    kienquoc

--- bflow_ai/app/api/endpoints.py
+++ bflow_ai/app/api/endpoints.py
@@ -2,12 +2,16 @@ from fastapi import APIRouter, HTTPException
 from app.models.schemas import QueryRequest, AIResponse, DocumentSchema
 from app.services.rag_service import RAGService
 from app.db.mongodb import get_database
+from app.services.rag_service_test import RagAccounting
 
 router = APIRouter()
 rag_service = RAGService()
 
 
 @router.get("/ask")
-async def ask_ai(question: str, top_k: int = 3):
-    request = QueryRequest(question=question, top_k=top_k)
-    return await rag_service.generate_answer(request)
+async def ask_ai(question: str):
+    request = question
+    # return await rag_service.generate_answer(request)
+    answer_json = RagAccounting.rag_accounting(user_input=request)
+    answer_text = RagAccounting.synthesize_answer(request, answer_json)
+    return answer_text
